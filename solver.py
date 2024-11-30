339843491cf186c037517528d6fc032a54f697d9
ebravofm <ebravofm@gmail.com>
2024-11-30 13:42:56 -0300

    tex

--- solver.py
+++ solver.py
@@ -1,9 +1,49 @@
+import logging
+from datetime import datetime, timedelta
 from ortools.sat.python import cp_model
-from datetime import timedelta
+
+# Configuración básica del logger
+logging.basicConfig(
+    filename="solver.log",
+    level=logging.INFO,
+    format="%(message)s",
+    filemode="a"  # Abrir en modo append
+)
+
+def log_solver_run(
+    tasks, 
+    task_results, 
+    status, 
+    solve_time, 
+    makespan, 
+    objective_value,
+    solve_time_full
+):
+    """
+    Función para registrar los detalles de una corrida del solver en un archivo de log.
+    """
+    # Fecha y hora de la corrida
+    run_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Número de tareas
+    num_tasks = len(tasks)
+    
+    # Crear línea de log
+    log_line = (
+        f"{run_time}, "
+        f"Status: {status}, "
+        f"Tasks: {num_tasks}, "
+        f"Makespan: {makespan} hours, "
+        f"Objective Value: {objective_value}, "
+        f"Solve Time: {solve_time:.2f} seconds"
+        f"Solve Time: {solve_time_full:.2f} seconds"
+    )
+    
+    # Escribir en el log
+    logging.info(log_line)
 
 
 def solve(tasks, task_windows, task_groups, max_impact, resource_capacities, resources_forbidden_intervals, min_date, scaling):
-
     # Definir el modelo de optimización
     model = cp_model.CpModel()
     is_scheduled = {}
@@ -20,27 +60,6 @@ def solve(tasks, task_windows, task_groups, max_impact, resource_capacities, res
         task_starts[task_id] = start_var
         task_ends[task_id] = end_var
         task_intervals[task_id] = interval_var
-        
-        
-        # Añadir restricciones para evitar que las tareas se programen durante los intervalos prohibidos
-        for resource_id in required_resources:
-            for forbidden_start, forbidden_end in resources_forbidden_intervals[resource_id]:
-                # Variables booleanas que indican si la tarea termina antes o comienza después del intervalo prohibido
-                before_interval = model.NewBoolVar(f'before_{task_id}_{forbidden_start}_{forbidden_end}')
-                after_interval = model.NewBoolVar(f'after_{task_id}_{forbidden_start}_{forbidden_end}')
-
-                # Si 'before_interval' es verdadero, la tarea termina antes de que comience el intervalo prohibido
-                model.Add(end_var <= forbidden_start).OnlyEnforceIf(before_interval)
-                # Si 'after_interval' es verdadero, la tarea comienza después de que termina el intervalo prohibido
-                model.Add(start_var >= forbidden_end).OnlyEnforceIf(after_interval)
-
-                # La tarea debe estar programada fuera del intervalo prohibido si está programada
-                model.AddBoolOr([before_interval, after_interval, is_scheduled[task_id].Not()])
-
-                # Enlazar 'before_interval' y 'after_interval' con 'is_scheduled' para evitar programar tareas no planificadas
-                model.Add(before_interval == 0).OnlyEnforceIf(is_scheduled[task_id].Not())
-                model.Add(after_interval == 0).OnlyEnforceIf(is_scheduled[task_id].Not())
-        
 
     # Añadir restricciones cumulativas para cada recurso
     for resource_id, (capacity, type) in resource_capacities.items():
@@ -50,71 +69,68 @@ def solve(tasks, task_windows, task_groups, max_impact, resource_capacities, res
             if resource_id in required_resources:
                 intervals.append(task_intervals[task_id])
                 if type == 0:
-                    demands.append(cantidad)  # Cada tarea usa 1 unidad de capacidad
+                    demands.append(cantidad)
                 else:
-                    demands.append(1)  # Cada tarea usa 1 unidad de capacidad
-        # Capacidad del recurso establecida a 1
+                    demands.append(1)
         model.AddCumulative(intervals, demands, capacity)
 
-
-    # Crear variables de programación para los grupos (Precedencia dentro de TaskGroup)
+    # Crear variables de programación para los grupos
     group_scheduled = {}
-    # Restricciones de TaskGroup
     for group_id, group in task_groups.items():
-        # Crear una variable booleana para el grupo
         group_var = model.NewBoolVar(f'group_scheduled_{group_id}')
         group_scheduled[group_id] = group_var
-
-        # Vincular las variables 'is_scheduled' de las tareas con la variable del grupo
         for task_id in group:
             model.Add(is_scheduled[task_id] == group_var)
 
-        # Agregar restricciones de precedencia entre tareas consecutivas dentro del grupo
-        previous_task_id = None
-        for task_id in group:
-            if previous_task_id is not None:
-                # Si el grupo está programado, asegurar la precedencia entre tareas
-                model.Add(task_starts[task_id] >= task_starts[previous_task_id]+tasks[previous_task_id][0]).OnlyEnforceIf(group_var)
-            previous_task_id = task_id
-            
-    # Calcular pesos inversos basados en 'Impact' (1 es más prioritario)
+    # Calcular pesos inversos basados en 'Impact'
     weights = {task_id: (max_impact + 1 - tasks[task_id][3]) ** 3 for task_id in tasks}
 
     # Definir la función objetivo ponderada
     model.Minimize(sum(weights[task_id] * (1 - is_scheduled[task_id]) for task_id in tasks))
 
     solver = cp_model.CpSolver()
-    #solver.parameters.log_search_progress = True
-    solver.parameters.max_time_in_seconds = 10  # Establece el límite de tiempo a 60 segundos (ajusta según tus necesidades)
-    solver.parameters.relative_gap_limit = 0.25  # Define un gap aceptable del 25%
-    
-    
-    #solver.parameters.stop_after_first_solution = True
-    #solver.parameters.search_branching = cp_model.FIXED_SEARCH  
-      
-    #print(1)
-    status = solver.Solve(model)
-    
-    # Verificar si se encontró una solución
-    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        task_results = {}
-        #print('Solución Encontrada.')
+    solver.parameters.max_time_in_seconds = 10
+    solver.parameters.relative_gap_limit = 0.25
 
-        # Iterar sobre TaskIDs para recopilar resultados
-        for task_id in task_starts.keys():  # task_starts debe estar definido previamente
+    # Medir tiempo de solución
+    start_timestamp = datetime.now()
+    status = solver.Solve(model)
+    solve_time = (datetime.now() - start_timestamp).total_seconds()
+
+    # Procesar resultados
+    task_results = {}
+    makespan = 0
+    total_task_group_duration = 0
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        for task_id, start_var in task_starts.items():
             scheduled = solver.Value(is_scheduled[task_id])
-            start_time = solver.Value(task_starts[task_id]) if scheduled else None  # Solo tiene sentido si está programada
-            
+            start_time = solver.Value(start_var) if scheduled else None
+            end_time = solver.Value(task_ends[task_id]) if scheduled else None
+            if scheduled and end_time:
+                makespan = max(makespan, end_time)
             task_results[task_id] = {
                 "Scheduled": scheduled,
-                "Start": (min_date + timedelta(hours=start_time/scaling)).isoformat() if scheduled else None
+                "Start": (min_date + timedelta(hours=start_time / scaling)).isoformat() if scheduled else None
             }
     else:
-        #print('No se encontró una solución factible.')
-        for task_id in tasks.keys():  # task_starts debe estar definido previamente            
-            task_results[task_id] = {
-                "Scheduled": 0,
-                "Start": None
-            }
-        
+        for task_id in tasks.keys():
+            task_results[task_id] = {"Scheduled": 0, "Start": None}
+            
+    solve_time_full = (datetime.now() - start_timestamp).total_seconds()
+
+
+    # Calcular valor de la función objetivo
+    objective_value = solver.ObjectiveValue()
+
+    # Escribir en el log
+    log_solver_run(
+        tasks,
+        task_results,
+        solver.StatusName(status),
+        solve_time,
+        makespan / scaling,
+        objective_value,
+        solve_time_full
+    )
+
     return task_results
